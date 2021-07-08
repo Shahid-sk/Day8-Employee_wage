193cbecd584533963f16c34cfd8b4516abab71e5
Shahid-sk <83329724+Shahid-sk@users.noreply.github.com>
2021-07-08 17:18:20 +0530

    Update Empwage.java

--- src/empwage/Empwage.java
+++ src/empwage/Empwage.java
@@ -1,8 +1,8 @@
 package empwage;
 
 public class Empwage {
-	int IS_EMP_PRESENT = 1;
 	public static void main(String[] args) {
+		int IS_EMP_PRESENT = 1;
 		System.out.println("Welcome to Employee wage simulation program");
 		double empcheck=Math.floor(Math.random()*10)%2;
 		if(empcheck == IS_EMP_PRESENT)
